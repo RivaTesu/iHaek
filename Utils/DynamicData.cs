7a821d41171e7cbf09bba210dbbada7ef4392598
Daniel Jacob <danieljacobowp@gmail.com>
2022-09-29 12:49:29 -0300

    Update DynamicData.cs
    Structure path information.

--- Utils/DynamicData.cs
+++ Utils/DynamicData.cs
@@ -9,7 +9,7 @@ public class DynamicData
         var dwBuildNumber = Marshal.ReadInt32((IntPtr)0x7FFE0260);
         switch (dwBuildNumber)
         {
-			case 19044: // W10
+            case 19044: // W10
             case 22000: // W11
                 ObjectTable = 0x570;
                 break;
@@ -20,5 +20,5 @@ public class DynamicData
     }
     // https://www.vergiliusproject.com/kernels/x64
 
-    public uint ObjectTable { get; } // struct _HANDLE_TABLE* ObjectTable;
+    public uint ObjectTable { get; } // (struct _EPROCESS) - struct _HANDLE_TABLE* ObjectTable;
 }
